d1807ccd92ca218c63db228913e98385285ad5db
Sudhardeena <sudharsanam16499@gmail.com>
2024-11-18 00:12:49 +0530

    modified db connection url

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Email configuration (using Mailtrap)
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+# # Email configuration (using Mailtrap)
+# app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+# app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+# app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
+# app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 # Initialize extensions
 db.init_app(app)
-mail.init_app(app)
+# mail.init_app(app)
 migrate.init_app(app, db)
 
 # Import and register routes after app initialization to avoid circular import
